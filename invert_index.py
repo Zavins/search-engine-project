884f1b6fdbb63177bc6a57f7affbb03adcd1b7db
ruixiaz1 <ruixiaz1@uci.edu>
2022-11-18 13:24:41 -0800

    update a invert bug

--- invert_index.py
+++ invert_index.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import TfidfVectorizer
 from simhash import Simhash, SimhashIndex
@@ -16,9 +15,9 @@ url_count = 0
 def process_content(content):
     soup = BeautifulSoup(content, "html.parser")
     content = soup.get_text()
-    tokens:list[str] = TokenHelper.tokenize(content)
+    tokens: list[str] = TokenHelper.tokenize(content)
     important_content = soup.find_all(['h1', 'h2', 'h3', 'b', 'a', 'title'], text=True)
-    #add weights for important contents.
+    # add weights for important contents.
     for c in important_content:
         print(c.string)
         tokens.extend(TokenHelper.tokenize(c.string))
@@ -45,9 +44,9 @@ def create_indexes(folder_path):
     global url_count
     f_iter = FileHelper.iterate_json_files(folder_path)
     for filename, url, content, encoding in f_iter:
-        doc_id:str = filename.rstrip(".json")
+        doc_id: str = filename.rstrip(".json")
         tokens = process_content(content)
-        #if there is similar document exist
+        # if there is similar document exist
         hash = Simhash(value=tokens, f=64)
         if len(simhash_index.get_near_dups(hash)) < 1:
             print(url_count, url)
@@ -58,21 +57,22 @@ def create_indexes(folder_path):
             doc_id_dict[url_count] = url
             add_to_dict(tf_idf, url_count)
 
+
 def save_indexes(file_path):
     index_dict = {}
-    i = 0
+    i = 1
     for k, v in inverted_index_dict.items():
         index_dict[k] = i
         v.sort(key=lambda x: x.doc_id)
         with open(file_path, "a") as f:
-            f.write(str(v)+"\n")
+            f.write(str(v) + "\n")
         i += 1
     FileHelper.save_json("./indexes.json", index_dict)
+
 
 def generate_output():
     print("Token number:", len(inverted_index_dict.keys()))
     print(f"Total inverted url: {url_count}")
-    
 
 
 if __name__ == '__main__':
